e020050cad04814b935fd4b25d8daa125aead636
Simon-Mategen <simon.mategen@gmail.com>
2022-05-12 13:40:26 +0200

    Created initial App structure

--- react-movielist/src/App.js
+++ react-movielist/src/App.js
@@ -1,6 +1,13 @@
 function App() {
   return (
     <div className="App">
+      <h1>Min Filmlista</h1>
+      <h3>Lägg till en film</h3>
+      <hr></hr>
+
+      {/* Form */}
+
+      {/* List */}
 
     </div>
   );
