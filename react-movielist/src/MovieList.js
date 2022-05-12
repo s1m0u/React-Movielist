86996e5a7560a83f644a7faaf22037298eeb322f
Simon-Mategen <simon.mategen@gmail.com>
2022-05-12 14:46:44 +0200

    Created MovieListItem

--- react-movielist/src/MovieList.js
+++ react-movielist/src/MovieList.js
@@ -1,11 +1,12 @@
 import React, { useState, useRef } from 'react';
+import MovieListItem from './MovieListItem';
 
 export default function MovieList() {
     return(
         <div>
             <h1>Inlagda filmer</h1>
             <ul className='list-group'>
-                <li className='list-group-item'>Temp</li>
+                <MovieListItem />
             </ul>
         </div>
     )
---
+++ react-movielist/src/MovieListItem.js
@@ -0,0 +1,9 @@
+import React, { useState, useRef } from 'react';
+
+export default function MovieListItem() {
+    return(
+        <div>
+            
+        </div>
+    )
+};
