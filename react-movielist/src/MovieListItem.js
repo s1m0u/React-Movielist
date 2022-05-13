0edc78b0366f48caabdf4f99c4d8cc830f55d2e6
Simon-Mategen <simon.mategen@gmail.com>
2022-05-13 11:18:48 +0200

    Style fix nr 2

--- react-movielist/src/MovieListItem.js
+++ react-movielist/src/MovieListItem.js
@@ -6,7 +6,7 @@ import './style.css';
 export default function MovieListItem(props) {
     return(
         <div>
-            <li className='list-group-item'>
+            <li className='list-group-item list-group-item-secondary'>
                 {props.item.title}
                 <img id='delete-icon' src={Delete} />
                 <img id='star-icon' src={Star} />
