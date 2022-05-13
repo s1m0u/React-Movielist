2b42cee7402e413818c648683bedcc948785da00
Simon-Mategen <simon.mategen@gmail.com>
2022-05-13 13:13:32 +0200

    Cleanup MovieList.js

--- react-movielist/src/MovieList.js
+++ react-movielist/src/MovieList.js
@@ -1,14 +1,12 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import MovieListItem from './MovieListItem';
 
 export default function MovieList(props) {
-    const movies = props.movies;
-
     return(
         <div>
             <h1>Inlagda filmer</h1>
             <ul className='list-group'>
-                {movies.map(movie => <MovieListItem key={movie.id} item={movie} deleteMovie={props.deleteMovie}/>)}
+                {props.movies.map(movie => <MovieListItem key={movie.id} item={movie} deleteMovie={props.deleteMovie}/>)}
             </ul>
 
             <button type="button" class="btn btn-primary" onClick={() => {props.alphabeticalSort()}}>Alfabetsik ordning</button>
